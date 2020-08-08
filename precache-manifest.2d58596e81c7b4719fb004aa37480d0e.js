46061f0d9660a649f7ca4dc2c0d3bef73105624b
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-08-08 16:37:31 -0500

    Updates

---
+++ precache-manifest.2d58596e81c7b4719fb004aa37480d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc31325e2d9256764b00b8adca320132",
+    "url": "/react-firebase-chat/index.html"
+  },
+  {
+    "revision": "b9ba1a8e8581d001693d",
+    "url": "/react-firebase-chat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d4c206d3f96cc849120",
+    "url": "/react-firebase-chat/static/js/2.688d9073.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-firebase-chat/static/js/2.688d9073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ba1a8e8581d001693d",
+    "url": "/react-firebase-chat/static/js/main.d737cc6f.chunk.js"
+  },
+  {
+    "revision": "e25c0b5c66213fe22b66",
+    "url": "/react-firebase-chat/static/js/runtime-main.d3c908d7.js"
+  }
+]);
